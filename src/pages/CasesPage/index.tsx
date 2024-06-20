d155c727e46d2dc9eb735aa3c45070874cced132
Spandan Barve <spandan567@gmail.com>
2024-06-20 14:56:29 +0530

    Add case page

--- src/pages/CasesPage/index.tsx
+++ src/pages/CasesPage/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import CasesTable from "./components/CasesPage";
+import CasesTable from "./components/CasesTable";
 
 export default function () {
   return (

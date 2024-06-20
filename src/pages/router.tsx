eeec2ff53be43f1bd0d2a07ad0ae762ccb590cc1
Riya Jain <riya1jain567@gmail.com>
2024-06-20 15:35:10 +0530

    resolve conflict

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -8,11 +8,8 @@ import Layout from "../layout";
 import HomePage from "./HomePage";
 import CasesPage from "./CasesPage";
 import ErrorPage from "./ErrorPage/ErrorPage";
-<<<<<<< HEAD
 import StakePage from "./StakePage";
-=======
 import CasePage from "./CasePage";
->>>>>>> e9b2f2cbaf7ed32fd9e84f0f3600329daea810a4
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,11 +17,8 @@ const router = createBrowserRouter(
       <Route element={<Layout.Default />}>
         <Route index element={<HomePage />} />
         <Route path="cases" element={<CasesPage />} />
-<<<<<<< HEAD
         <Route path="stake" element={<StakePage />} />
-=======
         <Route path="cases/:id" element={<CasePage />} />
->>>>>>> e9b2f2cbaf7ed32fd9e84f0f3600329daea810a4
       </Route>
 
       <Route path="*" element={<ErrorPage />} />

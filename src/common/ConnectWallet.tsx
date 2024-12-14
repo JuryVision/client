f86286a54e7f28aaf5a5b8bc9016bbb5af1dd223
Riya Jain <riya1jain567@gmail.com>
2024-12-14 19:06:57 +0800

    Add forward link

--- src/common/ConnectWallet.tsx
+++ src/common/ConnectWallet.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ConnectWallet() {
   
   return (
     <div className="m-10">
-      <button className="before:absolute-cover relative overflow-hidden rounded-full bg-gradient-to-bl from-primary to-transparent px-10 py-3 font-raleway font-semibold tracking-wider text-front
+      <Link to="https://opticks-dapp.vercel.app" className="whitespace-nowrap before:absolute-cover relative overflow-hidden rounded-full bg-gradient-to-bl from-primary to-transparent px-10 py-3 font-raleway font-semibold tracking-wider text-front
       duration-500 before:-z-1 before:bg-primary before:opacity-10 before:duration-inherit hover:scale-90 hover:text-back hover:opacity-70 hover:before:opacity-100">
       Launch Dapp
-    </button>
+    </Link>
     </div>
   );
 }

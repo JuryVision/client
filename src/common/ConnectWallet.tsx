9402dc9b0725a58a50c5f9a9bed01755acdf9550
Riya Jain <riya1jain567@gmail.com>
2024-12-14 19:20:06 +0800

    change deployment link

--- src/common/ConnectWallet.tsx
+++ src/common/ConnectWallet.tsx
@@ -5,7 +5,7 @@ export default function ConnectWallet() {
 
   return (
     <div className="m-10">
-      <Link to="https://opticks-dapp.vercel.app" >
+      <Link to="https://opticks-dapp.netlify.app/" >
         <button className="whitespace-nowrap before:absolute-cover relative overflow-hidden rounded-full bg-gradient-to-bl from-primary to-transparent px-10 py-3 font-raleway font-semibold tracking-wider text-front
       duration-500 before:-z-1 before:bg-primary before:opacity-10 before:duration-inherit hover:scale-90 hover:text-back hover:opacity-70 hover:before:opacity-100">
           Launch Dapp
